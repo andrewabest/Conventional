f2aa7145cf541311787ed8a30d7e2c6710ac87ba
Andrew Best <andrew.best@readify.net>
2015-01-04 13:22:08 +1000

    Added nuspec so it can be uploaded to the gallery.

--- Conventional/Properties/AssemblyInfo.cs
+++ Conventional/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Conventional")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A suite of convention specifications for enforcing type conventions in your codebase")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Andrew Best")]
 [assembly: AssemblyProduct("Conventional")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
