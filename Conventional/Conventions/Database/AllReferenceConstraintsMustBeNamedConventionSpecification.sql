1b4b2f287bde47ccb1cf9a393bcfd87f9941facc
Chui Tey <chui.tey@readify.net>
2016-06-05 20:05:22 +1000

    Foreign keys must be named explicitly.

--- Conventional/Conventions/Database/AllReferenceConstraintsMustBeNamedConventionSpecification.sql
+++ Conventional/Conventions/Database/AllReferenceConstraintsMustBeNamedConventionSpecification.sql
@@ -1,3 +1,4 @@
-﻿SELECT constraint_name
-FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS
-WHERE constraint_name LIKE '%[_][_][0-9A-F][0-9A-F][0-9A-F][0-9A-F][0-9A-F][0-9A-F][0-9A-F][0-9A-F]'
+﻿SELECT name
+FROM sys.foreign_keys
+WHERE is_system_named = 1
+ORDER BY name
