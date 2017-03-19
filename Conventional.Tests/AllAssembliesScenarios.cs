df4863a529f267dd70d08fdaa9847ae8aafd853e
Andrew Best <andrew.best@readify.net>
2017-03-19 14:34:59 +1000

    Adds AllAssemblies specimen builder and Conformist extension

---
+++ Conventional.Tests/AllAssembliesScenarios.cs
@@ -0,0 +1,16 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Conventional.Tests
+{
+    public class AllAssembliesScenarios
+    {
+        [Test]
+        public void GivenAPattern_LocatesAndReturnsAllAssembliesForThatPattern()
+        {
+            var assemblySpecimen = AllAssemblies.WithNamesMatching("*");
+
+            assemblySpecimen.Should().HaveCount(8);
+        }
+    }
+}
---
+++ Conventional/AllAssemblies.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Linq;
+using Conventional.Conventions.Assemblies;
+
+namespace Conventional
+{
+    public static class AllAssemblies
+    {
+        public static AssemblySpecimen[] WithNamesMatching(string pattern)
+        {
+            if (pattern.EndsWith(".csproj") == false)
+            {
+                pattern += ".csproj";
+            }
+
+            var candidates = Directory.GetFiles(KnownPaths.SolutionRoot, pattern, SearchOption.AllDirectories);
+
+            if (candidates.Any() == false)
+            {
+                throw new ConventionException("Could not locate a project file matching the pattern {0}".FormatWith(pattern));
+            }
+
+            return candidates.Select(x => new AssemblySpecimen(x)).ToArray();
+        }
+    }
+}
