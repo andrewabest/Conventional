88a2bfc80fa9b7d6fbea339640daac7f9743caae
Andrew Best <andrew.best@octopus.com>
2022-11-16 10:29:35 +1030

    Ensures interface assignments are appropriately delved

---
+++ src/Core/Conventional.Tests/Conventional/Conventions/Cecil/CecilExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Conventional.Conventions.Cecil;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Conventional.Tests.Conventional.Conventions.Cecil
+{
+    public class CecilExtensionsTests
+    {
+        private class HasBaseInterface : List<string> {}
+        
+        private class DerivedFromDerieved : Derived {}
+        
+        private class Derived : Base{}
+        
+        private class Base {}
+        
+        [Test]
+        public void IsAssignableTo_DelvesTypeHeirarchy_ToFindInterfaces()
+        {
+            typeof(HasBaseInterface).ToTypeDefinition().IsAssignableTo(typeof(IEnumerable)).Should().BeTrue();
+        }
+        
+        [Test]
+        public void IsAssignableTo_DelvesTypeHeirarchy_ToFindBases()
+        {
+            typeof(DerivedFromDerieved).ToTypeDefinition().IsAssignableTo(typeof(Base)).Should().BeTrue();
+        }
+    }
+}
