c5fecc57bb46e1c0f8b17b214011f813c3454b5f
Andrew Best <andrew.best@octopus.com>
2022-11-16 10:31:11 +1030

    Ensures interface assignments are appropriately delved (#77)

--- src/Core/Conventional/Conventions/Cecil/CecilExtensions.cs
+++ src/Core/Conventional/Conventions/Cecil/CecilExtensions.cs
@@ -10,26 +10,29 @@ namespace Conventional.Conventions.Cecil
     {
         public static bool IsAssignableTo(this TypeDefinition type, Type derivedType)
         {
-            return
-                IsAssignableToInterface(type, derivedType) || IsAssignableToBase(type, derivedType);
+            return derivedType.IsInterface
+                ? IsAssignableToInterface(type, derivedType)
+                : IsAssignableToBase(type, derivedType);
         }
 
         public static bool IsAssignableToInterface(this TypeDefinition type, Type derivedType)
         {
             var cecilFormattedTypeName = derivedType.FullName.Replace("+", "/");
 
-            return type.Interfaces.Any(
-                i =>
-                    i.InterfaceType.FullName.Equals(cecilFormattedTypeName) ||
-                    i.InterfaceType.Resolve().Interfaces.Any(x => IsAssignableToInterface(x.InterfaceType.Resolve(), derivedType)));
+            return 
+                type.Interfaces.Any(
+                    i =>
+                        i.InterfaceType.FullName.Equals(cecilFormattedTypeName) ||
+                        i.InterfaceType.Resolve().Interfaces.Any(x => IsAssignableToInterface(x.InterfaceType.Resolve(), derivedType))) ||
+                (type.BaseType != null && type.BaseType.Resolve().IsAssignableToInterface(derivedType));
         }
         
         public static bool IsAssignableToBase(this TypeDefinition type, Type derivedType)
         {
             var cecilFormattedTypeName = derivedType.FullName.Replace("+", "/");
 
-            return (type.BaseType != null && type.BaseType.FullName.Equals(cecilFormattedTypeName)) ||
-                   (type.BaseType != null && IsAssignableToBase(type.BaseType.Resolve(), derivedType));
+            return type.BaseType != null &&
+                   (type.BaseType.FullName.Equals(cecilFormattedTypeName) || IsAssignableToBase(type.BaseType.Resolve(), derivedType));
         }
 
         public static IEnumerable<PropertyDefinition> GetPropertiesOfType(this TypeDefinition typeDefinition, Type type)
