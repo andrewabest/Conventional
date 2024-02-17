05c67bfcc447c9cc2ed7453425ac11426cf9856c
eddie.stanley <eddie.stanley@xero.com>
2024-02-16 18:32:39 -0800

    [GH-87] Adds Convention.NamespaceMustStartWith & Convention.NamespaceMustEndWith (which were discussed but not implemented under pull-request GH-90)
    Currently (as of 11.0.11) there's only
    - Convention.NameMustStartWith / Convention.NameMustEndWith (which only consider the immediate type name, not FullName) and
    - Convention.MustLiveInNamespace (which doesn't support a prefix/suffix/regex)

---
+++ src/Core/Conventional/Conventions/NamespaceMustEndWithConventionSpecification.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Conventional.Conventions
+{
+    public class NamespaceMustEndWithConventionSpecification : ConventionSpecification
+    {
+        private readonly string _namespaceSuffix;
+
+        public NamespaceMustEndWithConventionSpecification(string suffix) => _namespaceSuffix = suffix;
+
+        protected override string FailureMessage => "Must live in namespace ending with {0} but actually lives in namespace {1}";
+
+        public override ConventionResult IsSatisfiedBy(Type type)
+        {
+            if (type.Namespace != null && type.Namespace.EndsWith(_namespaceSuffix))
+            {
+                return ConventionResult.Satisfied(type.FullName);
+            }
+
+            return ConventionResult.NotSatisfied(type.FullName, FailureMessage.FormatWith(_namespaceSuffix, type.Namespace));
+        }
+    }
+}
